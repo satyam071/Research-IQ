56b91b55f19d11b1bf1cfcae488637ec26593f60
unknown <ysatyam0071@gmail.com>
2026-07-30 09:38:39 +0530

    Final Commit, Thank You!:)

--- src/Components/ChatSection.tsx
+++ src/Components/ChatSection.tsx
@@ -24,43 +24,43 @@ const ChatSection: React.FC<Props> = () => {
                 <ChattingComponent />
 
             ) :
-                    <div className=" flex flex-col justify-evenly ">
-                        <h1 className="font-archivo  text-2xl text-center mt-5 tracking-wider uppercase">Select the Mode</h1>
-                        <InfoBox
-                            className="bg-[#2BB4A0] text-black "
-                            title="Paper Mode"
-                            description=
-                                "Analyze the research paper in a structured way,
-                                providing summary, key points, methodology, results,
-                                and important findings."
-                            button_title="Set paper Mode"
-                            onClick={() => { setMode('paper') }}
-                        /> <InfoBox
-                            className="bg-[#F2E6CF] text-black"
-                            title="Hybrid Mode"
-                            description="Combines paper analysis with Al assistance to
+                <div className=" flex flex-col justify-evenly ">
+                    <h1 className="font-archivo  text-2xl text-center mt-5 tracking-wider uppercase">Select the Mode</h1>
+                    <InfoBox
+                        className="bg-[#2BB4A0] text-black "
+                        title="Paper Mode"
+                        description=
+                        {`Analyze the research paper in a structured way,
+                            providing summary, key points, methodology, results,
+                            and important findings.`}
+                        button_title="Set paper Mode"
+                        onClick={() => { setMode("paper") }}
+                    /> <InfoBox
+                        className="bg-[#F2E6CF] text-black"
+                        title="Hybrid Mode"
+                        description="Combines paper analysis with Al assistance to
                                     provide deeper insights, explanations, and
                                     connections between concepts."
-                            button_title="Set Hybrid Mode"
-                            onClick={() => { setMode('hybrid') }}
-                        />
-                         <InfoBox
-                            className="bg-[#161B22] text-white"
-                            
-                            title="Explain Mode"
-                            description="Simplifies complex research content and explains it
+                        button_title="Set Hybrid Mode"
+                        onClick={() => { setMode('hybrid') }}
+                    />
+                    <InfoBox
+                        className="bg-[#161B22] text-white"
+
+                        title="Explain Mode"
+                        description="Simplifies complex research content and explains it
                                 in an easy-to-understand manner for better
                                 learning."
-                            button_title="Set Explain Mode"
-                            button_line_color="after:bg-white"
-                            onClick={() => { setMode('explain') }}
-                        />
+                        button_title="Set Explain Mode"
+                        button_line_color="after:bg-white"
+                        onClick={() => { setMode('explain') }}
+                    />
 
 
 
 
-                    </div>
-                    
+                </div>
+
 
             }
         </>
--- src/Components/InfoBox.tsx
+++ src/Components/InfoBox.tsx
@@ -8,17 +8,17 @@ interface Props {
     button_title: React.ReactNode
     className?: string
     onClick: React.MouseEventHandler<HTMLButtonElement>;
-    button_line_color?:React.ReactNode
+    button_line_color?: string;
 
 
 }
 
 
-const InfoBox: React.FC<Props> = ({ logo, description, title, button_title, button_line_color="after:bg-black",onClick, className }) => {
+const InfoBox: React.FC<Props> = ({ logo, description, title, button_title, button_line_color = "after:bg-black", onClick, className }) => {
 
-    const {theme}=useContext(ThemeContextData)
+    const { theme } = useContext(ThemeContextData)
     return (
-        <button onClick={onClick} className={`m-3 col-span-12 lg:col-span-4 group  border-4 border-black p-6  transition-all duration-300 cursor-pointer ${className}  ${theme=='light'?"shadow-[7px_7px_0px_#000]":"shadow-[7px_7px_0px_#fff]"}`}>
+        <button onClick={onClick} className={`m-3 col-span-12 lg:col-span-4 group  border-4 border-black p-6  transition-all duration-300 cursor-pointer ${className}  ${theme == 'light' ? "shadow-[7px_7px_0px_#000]" : "shadow-[7px_7px_0px_#fff]"}`}>
 
 
             <h2 className=" font-mono text-lg  tracking-wide font-bold uppercase  text-center items-center">
@@ -30,12 +30,14 @@ const InfoBox: React.FC<Props> = ({ logo, description, title, button_title, butt
                 {description}
             </p>
 
-            <button className={`mt-3 uppercase relative font-mono text-sm font-bold after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0  after:transition-all group-hover:after:w-full
-            ${button_line_color}
-                
-                `}>
+            <span
+                className={`mt-3 inline-block uppercase relative font-mono text-sm font-bold
+                    after:absolute after:left-0 after:-bottom-1 after:h-[2px]
+                    after:w-0 after:transition-all group-hover:after:w-full
+                    ${button_line_color}`}
+                                >
                 {button_title}
-            </button>
+            </span>
 
         </button>
     );
